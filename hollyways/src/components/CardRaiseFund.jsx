1139b089be81c63865f869cf55613c502d925e0d
nfernando0 <nandof986@gmail.com>
2023-07-31 22:23:46 +0700

    commit

--- hollyways/src/components/CardRaiseFund.jsx
+++ hollyways/src/components/CardRaiseFund.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
 import { Button, Card, Col, Container, ProgressBar, Row } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import { FormatRupiah } from "@arismun/format-rupiah";
 import Fund from '../assets/images/fund.png'
+import { UserContext } from '../context/UserContext';
+import ModalLogin from './modals/ModalLogin';
 
 const MyRaiseFundComponent = ({ fund }) => {
+
+    const [state, dispatch] = useContext(UserContext)
+
+    const [showLogin, setShowLogin] = useState(false);
+    const handleCloseLogin = () => setShowLogin(false);
+    const handleShowLogin = () => {
+        handleCloseRegister(false);
+        setShowLogin(true);
+    }
+
+    const [showRegister, setShowRegister] = useState(false);
+    const handleCloseRegister = () => setShowRegister(false);
+    const handleShowRegister = () => {
+        handleCloseLogin(false);
+        setShowRegister(true);
+    }
 
     // console.log(fund?.fundraising?.title)
     return (
@@ -23,12 +41,17 @@ const MyRaiseFundComponent = ({ fund }) => {
                                         {/* <p>{fund?.goals}</p> */}
                                     </FormatRupiah>
                                 </div>
-                                <Link to={`/raiseFund/${fund?.id}`} style={{ backgroundColor: 'var(--bg)', border: 'none', textDecoration: 'none', color: '#ffff', paddingLeft: '2rem', paddingRight: '2rem', paddingTop: '0.3rem', paddingBottom: '0.3rem', borderRadius: '0.5rem' }} >Donate Now</Link>
+                                {state.isLogin ? (
+                                    <Link to={`/raiseFund/${fund?.id}`} style={{ backgroundColor: 'var(--bg)', border: 'none', textDecoration: 'none', color: '#ffff', paddingLeft: '2rem', paddingRight: '2rem', paddingTop: '0.3rem', paddingBottom: '0.3rem', borderRadius: '0.5rem' }} >Donate Now</Link>
+                                ) : (
+                                    <Button variant='primary' onClick={handleShowLogin} style={{ backgroundColor: 'var(--bg)', border: 'none', textDecoration: 'none', color: '#ffff', paddingLeft: '2rem', paddingRight: '2rem', paddingTop: '0.3rem', paddingBottom: '0.3rem', borderRadius: '0.5rem' }}>Donate Now</Button>
+                                )}
                             </div>
                         </div>
                     </Card.Body>
                 </Card>
             </Col>
+            <ModalLogin show={showLogin} onHide={handleCloseLogin} />
         </div>
     )
 }
